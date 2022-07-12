64333e9ac53cea79184909fa38ff77d315ddfb00
Caroline Barbosa <bo.varig@gmail.com>
2022-07-11 22:08:02 -0300

    created the functions requested in the challenge

--- desafio-6/index.js
+++ desafio-6/index.js
@@ -1,4 +1,23 @@
-for (let index = 0; index <= 20; index++) {
-  const dobro = index * 2 
-  console.log('O dobro do n é ' + index + ' é ' + dobro)
+function somar (numero1, numero2) {
+  let soma = numero1 + numero2;
+  console.log('A soma dos dois números é: ' + soma);
 }
+somar(1,2);
+
+function subtrair (numero1, numero2) {
+  let subtrai = numero1 - numero2;
+  console.log('A subtração dos dois números é: ' + subtrai);
+}
+subtrair(10,5);
+
+function multiplicar (numero1, numero2) {
+  let multiplica = numero1 * numero2;
+  console.log('A multiplicação dos dois números é: ' + multiplica);
+}
+multiplicar(5,6);
+
+function dividir (numero1, numero2) {
+  let divide = numero1 / numero2;
+  console.log('A divisão dos dois números é: ' + divide);
+}
+dividir(30,3);
