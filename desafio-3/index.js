d60cad97351fe21639659c1d8bed88182c40082a
Caroline Barbosa <bo.varig@gmail.com>
2022-07-11 21:28:22 -0300

    fixing the response message as requested in the exercise

--- desafio-3/index.js
+++ desafio-3/index.js
@@ -1,8 +1,8 @@
 const hospedar = confirm('Seja bem vindo! Gostaria de passar a noite em nossa hospedagem?')
 
 if(hospedar) {
-  alert('Ótimo')
+  alert('Ótimo! Nós temos as melhores camas de toda a região!')
 } else {
-  alert('Que pena!')
+  alert('Que pena! Você parecia ser  uma pessoa mais legal')
 }
 
